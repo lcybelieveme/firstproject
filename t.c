99ee9a41779da9dcc1e495651339ca8692f56394
lcybelieveme <857929453@qq.com>
2016-04-15 00:51:02 -0700

    first program

---
+++ t.c
@@ -0,0 +1,12 @@
+ ///
+ /// @file    t.c
+ /// @author  lemon(haohb13@gmail.com)
+ /// @date    2016-04-15 00:48:38
+ /// 
+#include <stdio.h>
+
+int main()
+{
+	printf("This is my first program\n");
+	return 0;
+}
